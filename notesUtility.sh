8573d2150a0c23072ef02347ba10b2e84b3d0d89
Zafar Ansari <Zafar.Ansari@makemytrip.com>
2016-02-07 22:08:04 +0530

    removed extra newline

--- notesUtility.sh
+++ notesUtility.sh
@@ -8,7 +8,8 @@ j=1
 while [ "`xclip -selection clipboard -o`" != "" ]; do
 	if [ "$i" != "`xclip -selection clipboard -o`" ]; then
 		if [ $j -gt 1 ]; then  #Ignore what is already in the clipboard (for j=1)
-			echo "\n--------------Note $j---------------" >> "$path"newNote"$serial".txt
+			echo "Taken note"
+			echo "--------------Note $j---------------" >> "$path"newNote"$serial".txt
 			echo "Time: `date`" >> "$path"newNote"$serial".txt
 			echo "`xclip -selection clipboard -o`" >> "$path"newNote"$serial".txt
 		fi
