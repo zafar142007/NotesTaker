46b3dd1404a91c21ac730bb5021f0b53cd77b584
Zafar Ansari <Zafar.Ansari@makemytrip.com>
2016-02-07 18:50:59 +0530

    userpath separated out

--- notesUtility.sh
+++ notesUtility.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-mkdir ~/myNotes
-serial="$(find ~/myNotes/myNotes -type f | wc -l)"
-touch ~/myNotes/newNote"$serial".txt
+path=~/myNotes/
+mkdir $path
+serial="$(find $path -type f | wc -l)"
+touch "$path"newNote"$serial".txt
 j=1
 while [ "`xclip -selection clipboard -o`" != "" ]; do
 	if [ "$i" != "`xclip -selection clipboard -o`" ]; then
 		if [ $j -gt 1 ]; then  #Ignore what is already in the clipboard (for j=1)
-			echo "\n--------------Note $j---------------" >> newNote"$serial".txt
-			echo "Time: `date`" >> newNote"$serial".txt
-			echo "`xclip -selection clipboard -o`" >> newNote"$serial".txt
+			echo "\n--------------Note $j---------------" >> "$path"newNote"$serial".txt
+			echo "Time: `date`" >> "$path"newNote"$serial".txt
+			echo "`xclip -selection clipboard -o`" >> "$path"newNote"$serial".txt
 		fi
 		j=$((j+1))
 	fi
